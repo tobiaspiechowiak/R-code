169f3760972f1378a604a61569c13775273fae01
Tobias Piechowiak <tobias.piechowiak@gmail.com>
2017-02-28 08:48:07 +0100

    added Q4 (2D data) to lektion 4

--- Lektion 4/Program4_TPI.R
+++ Lektion 4/Program4_TPI.R
@@ -57,7 +57,7 @@ dev.off()
 #example index = ccc (Beale,1983) (15 cluster optimal, see pdf "Optimal number of cluster.pdf")
 fit.PAM.ccc <- pam(nwine,k = 15,stand = FALSE)
 library(cluster)  #load clusplot
-pdf("PAM of wine data with 15 cluster.pdf")
+pdf("PAM of wine data with 15 cluster.pdfwine data with 15 cluster.pdf")
 plot(fit.PAM.ccc,main="PAM wine data 15 cluster")
 
 dev.off()
@@ -123,22 +123,58 @@ ri.km.k3 <- randIndex(ct.km.k3) #rand Index for k-means with k = 3: 0.90
 #Q4#############################################
 #create the cluster
 #four cluster mean 1, 2, 3, 4; std 0.2, 1, 0.7, 2
-c1 <- cbind(rnorm(100,1,0.2), rnorm(100,1,0.2)) #x,y data cluster 1
-c2 <- cbind(rnorm(100,2,0.3), rnorm(100,2,0.3)) #x,y data cluster 2
-c3 <- cbind(rnorm(100,3,0.3), rnorm(100,3,0.2)) #x,y data cluster 3
-c4 <- cbind(rnorm(100,4,0.4), rnorm(100,1,0.4)) #x,y data cluster 3
+c1 <- cbind(rnorm(100,1,1), rnorm(100,1,1)) #x,y data cluster 1
+c1 <- cbind(rep.int(1,100),c1)
+c2 <- cbind(rnorm(100,2,1), rnorm(100,2,1)) #x,y data cluster 2
+c2 <- cbind(rep.int(2,100),c2)
+c3 <- cbind(rnorm(100,3,1), rnorm(100,3,1)) #x,y data cluster 3
+c3 <- cbind(rep.int(3,100),c3)
+c4 <- cbind(rnorm(100,4,1), rnorm(100,1,1)) #x,y data cluster 3
+c4 <- cbind(rep.int(4,100),c4)
 c5 <- rbind(c1,c2,c3,c4)
+
+#normalize the data 
+c5.scaled <- scale(c5[,2:3])
 
 #plot them 
 dev.new()
-plot(c5) #plot the cluster 
+plot(c5.scaled) #plot the cluster 
 title("4 cluster gaussian dist.")
 par(opar.org)
 
+#check first for cluster number in kmeans
+library(NbClust)
+nrCluster.kmeans <- NbClust(c5.scaled,distance = "euclidean",min.nc = 2,
+               max.nc = 15,method = "kmeans")
+pdf("Optimal number of cluster for Q4 (kmeans).pdf")
+barplot(nrCluster.kmeans$Best.nc[1,],xlab = "method",ylab = "nr cluster")
+dev.off()
 
+#Most metrics from NbClust propose 4 cluster for k-means
+#3 cluster 
+fit.km.4 <- kmeans(c5,4,nstart = 25)
+ct.km.4 <- table(c5[,1],fit.km.4$cluster) #cross-tabulation table for k = 5
+ri.km.4 <- randIndex(ct.km.4) #rand Index for k = 5: 0.53
 
+#check first for cluster number in PAM
+library(NbClust)
+nrCluster.pam <- NbClust(c5.scaled,distance = "euclidean",min.nc = 2,
+                     max.nc = 15,method = "median")
+pdf("Optimal number of cluster for Q4 (Pam).pdf")
+barplot(nrCluster.pam$Best.nc[1,],xlab = "method",ylab = "nr cluster")
+dev.off()
 
-
+#Most metrics from NbClust propose 4 cluster for PAM
+#4 cluster 
+fit.pam.4 <- pam(c5.scaled,k = 4, stand = FALSE)
+ct.pam.4 <- table(c5[,1],fit.pam.4$clustering) #cross-tabulation table for k = 5
+ri.pam.4 <- randIndex(ct.pam.4) #rand Index for k = 5: 0.53
+library(cluster)  #load clusplot
+pdf("PAM of 2-D data.pdf")
+clusplot(fit.pam.4,main="PAM of 2-D data with 4 cluster and st=1")
+dev.off()
+#k-means works better than PAM here. Both work really good for low st in the distributions.
+#Get worse with higher st. With larger st NbClust gets more ambiguous with them amount of cluster (3 preferred)
 ################################################
 
 
